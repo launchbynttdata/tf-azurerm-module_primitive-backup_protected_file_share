971830a024ea5948e0e939f0b70f40eff7df2b9d
Chandrabhanlanguriya-Ram-nttd <Chandrabhanlanguriya.Ram@nttdata.com>
2026-02-12 11:39:36 +0000

    initial code

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "azurerm_backup_protected_file_share" "backup_protected_file_share" {
-  count = var.enabled ? 1 : 0
 
   resource_group_name       = var.resource_group_name
   recovery_vault_name       = var.recovery_vault_name
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "protected_file_share_id" {
-  value = try(azurerm_backup_protected_file_share.backup_protected_file_share[0].id, null)
+  value = azurerm_backup_protected_file_share.backup_protected_file_share.id
 }
